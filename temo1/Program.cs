0404a688ef45116c022f4eae182a3e8b83029243
teimurazshengelia <teimurazshengelia@LHO20000342>
2024-10-23 18:04:42 +0400

    Add project files.

---
+++ temo1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.VisualBasic;
+
+Console.WriteLine("Hello");
+string name = "Temo";
+string lastname = "Shengelia";
+int age = 48;
+bool ismarryed = true;
+char initial = 't';
+float weight = 77.5f;
+double height = 170.2;
+Console.WriteLine($"my name is {name}; my lastname is {lastname}; i am {age} years old; is marryed {ismarryed}; my weight is {weight}; im am {height}");
